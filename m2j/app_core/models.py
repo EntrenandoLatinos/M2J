ef8255f5e73d3e783504c06e131e0ab78d6ee79e
CBM <camilobmoya21@gmail.com>
2024-03-01 00:40:07 -0500

    Merge conflicts solved V

--- m2j/app_core/models.py
+++ m2j/app_core/models.py
@@ -66,8 +66,8 @@ class About(AuditoriaFecha):
     about = RichTextField("About", null=True, blank=True)
     mision = RichTextField("Mission", null=True, blank=True)
     vision = RichTextField("Vision", null=True, blank=True)
-    image_google = models.ImageField(upload_to='about/', null=True, blank=True)
-    url_google = models.URLField("URL Google Business", null=True, blank=True)
+    # image_google = models.ImageField(upload_to='about/', null=True, blank=True)
+    # url_google = models.URLField("URL Google Business", null=True, blank=True)
 
     def __str__(self):
         return "{0}".format(str(self.id))
